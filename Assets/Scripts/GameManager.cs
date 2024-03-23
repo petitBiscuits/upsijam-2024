a5a129a25cdaee42aaad5bb64d774952fbc65b58
Ozurah <80265286+Ozurah@users.noreply.github.com>
2024-03-23 12:30:36 +0100

    Flux de l'eau + spawn des objets flotant

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -8,11 +9,32 @@ public class GameManager : MonoBehaviour
     #region Fields
     private int score = 0;
     [SerializeField] private List<FloatingObjectSO> availableFloatingObjectSO = new();
+    /// <summary>
+    /// Same index as <see cref="availableFloatingObjectSO"/>
+    /// </summary>
+    [SerializeField] private List<int> availableFloatingObjectSpawnWeight = new();
+    /// <summary>
+    /// If <see cref="availableFloatingObjectSpawnWeight"/> lenght is not enough, it will take this value
+    /// </summary>
+    [SerializeField] private int defaultSpawnWeight = 1;
+    [SerializeField] private float travelSpeed = 2;
+    [SerializeField] private float traveledDistance = 0;
+    [SerializeField] private float nextSpawnInDistance = 10;
+
+    [SerializeField] private int minSpawnInDistance = 5;
+    [SerializeField] private int maxSpawnInDistance = 15;
+
+    [SerializeField] private int spawnPositionOffset = 15;
+
     #endregion Fields
 
     #region Fields of Components
     [SerializeField] private Player player;
     #endregion Fields of Components
+
+    #region Fields of GameObjects
+    [SerializeField] private GameObject oceanGO;
+    #endregion Fields of GameObjects
 
     #region Properties
     public int Score
@@ -60,9 +82,57 @@ public class GameManager : MonoBehaviour
         
         Player.OnPlayerLife += OnPlayerLifeChange;
         Player.OnFloeLife += OnFloeLifeChange;
+    }
+
+    void FixedUpdate()
+    {
+        // Add default weight for missing ones
+        if (availableFloatingObjectSO.Count > availableFloatingObjectSpawnWeight.Count)
+        {
+            for (int i = 0; i < availableFloatingObjectSO.Count - availableFloatingObjectSpawnWeight.Count; i++)
+            { 
+                availableFloatingObjectSpawnWeight.Add(defaultSpawnWeight);
+            }
+        }
+
+        // Distance traveled
+        var traveled = travelSpeed * Time.fixedDeltaTime;
+        oceanGO.transform.Translate(Vector3.left * traveled);
+        traveledDistance += traveled;
+        nextSpawnInDistance -= traveled;
         
-        var floatingObject = Instantiate(availableFloatingObjectSO[0].prefab, new Vector3(0, 0, 0), Quaternion.identity);
-        FloatingObjects.Add(floatingObject, availableFloatingObjectSO[0]);
+        // Floating spawn
+        var leftScreenBorder = Camera.main.ViewportToWorldPoint(Vector3.zero).x;
+        var minSpawnY = Camera.main.ViewportToWorldPoint(new Vector3(0, 1)).y;
+        var maxSpawnY = Camera.main.ViewportToWorldPoint(new Vector3(0, 0)).y;
+
+        if (nextSpawnInDistance <= 0)
+        {
+            nextSpawnInDistance += UnityEngine.Random.Range(minSpawnInDistance, maxSpawnInDistance);
+
+            var totalWeight = availableFloatingObjectSpawnWeight.Sum(e => e);
+            var remainingWeight = UnityEngine.Random.Range(0, totalWeight) + 1;
+            
+            FloatingObjectSO objectToSpawn = availableFloatingObjectSO[0];
+            for (var i = 0; i < availableFloatingObjectSO.Count; i++)
+            {
+                var removedWeight = availableFloatingObjectSpawnWeight[i];
+
+                remainingWeight -= removedWeight;
+                if (remainingWeight <= 0)
+                {
+                    objectToSpawn = availableFloatingObjectSO[i];
+                    break;
+                }
+            }
+            
+
+            var position = new Vector3(leftScreenBorder + spawnPositionOffset, UnityEngine.Random.Range(minSpawnY, maxSpawnY), 0);
+            var floatingObject = Instantiate(objectToSpawn.prefab, position, Quaternion.identity);
+            floatingObject.transform.SetParent(oceanGO.transform);
+
+            FloatingObjects.Add(floatingObject, objectToSpawn);
+        }
     }
 
     private void OnFloeLifeChange(Player player, int before, int now)
